e2f5b2719d1c033624d93285627dbb8843012380
Fahima Begum <haffahima@gmail.com>
2023-05-29 19:48:44 +0100

    PK and FK added

--- src/main/resources/projects_schema.sql
+++ src/main/resources/projects_schema.sql
@@ -25,8 +25,7 @@ CREATE TABLE project_category (
   category_id INT NOT NULL, 
   FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
   FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
-  UNIQUE KEY (project_id, category_id)
-  
+  UNIQUE KEY (project_id, category_id)  
 );
 
 CREATE TABLE step (
@@ -35,7 +34,7 @@ CREATE TABLE step (
   step_text TEXT NOT NULL,
   step_order INT NOT NULL, 
   PRIMARY KEY (step_id),
-  FOREIGN KEY (project_id)
+  FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
 
 CREATE TABLE material (
@@ -45,5 +44,5 @@ CREATE TABLE material (
   num_required INT,
   cost DECIMAL(7,2),
   PRIMARY KEY (material_id),
-  FOREIGN KEY (project_id)
+  FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
